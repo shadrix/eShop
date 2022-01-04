801d21bec73cfc1d87dfdd470fc3b96a145e5d32
shadrix <postilga.dima@gmail.com>
2022-01-04 16:39:02 +0200

    added docker-compose

--- Catalog/Catalog.Host/Program.cs
+++ Catalog/Catalog.Host/Program.cs
@@ -1,26 +1,38 @@
-var builder = WebApplication.CreateBuilder(args);
+namespace Catalog.Host;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring
-// Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args);
+    }
+
+    private static void CreateHostBuilder(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring
+        // Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
