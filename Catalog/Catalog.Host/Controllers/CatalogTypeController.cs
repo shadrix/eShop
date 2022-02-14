1bcca0e361d57ab1d84fc7412504a40e705423ae
AntSenokosov <a.y.senokosov@gmail.com>
2022-02-14 21:06:21 +0200

    Add methods AddAsync, UpdateAsync and RemoveAsync in CatalogTypeController.cs

--- Catalog/Catalog.Host/Controllers/CatalogTypeController.cs
+++ Catalog/Catalog.Host/Controllers/CatalogTypeController.cs
@@ -1,3 +1,10 @@
+using System.Net;
+using Catalog.Host.Models.Requests;
+using Catalog.Host.Models.Response;
+using Catalog.Host.Services.Interfaces;
+using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Catalog.Host.Controllers;
 
 [ApiController]
@@ -5,9 +12,43 @@ namespace Catalog.Host.Controllers;
 public class CatalogTypeController : ControllerBase
 {
     private readonly ILogger<CatalogTypeController> _logger;
+    private readonly ICatalogTypeService _typeService;
 
-    public CatalogTypeController(ILogger<CatalogTypeController> logger)
+    public CatalogTypeController(
+        ILogger<CatalogTypeController> logger,
+        ICatalogTypeService typeService)
     {
         _logger = logger;
+        _typeService = typeService;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(AddTypeResponse<int?>), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> AddAsync(CreateTypeRequest request)
+    {
+        var res = await _typeService.AddAsync(request);
+        return Ok(new AddTypeResponse<int?>() { Id = res });
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(UpdateTypeResponse<int?>), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> UpdateAsync(UpdateTypeRequest request)
+    {
+        var res = await _typeService.UpdateAsync(request);
+        return Ok(new UpdateTypeResponse<int?>()
+        {
+            Result = res
+        });
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(RemoveTypeResponse<int?>), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> RemoveAsync(int typeId)
+    {
+        var res = await _typeService.RemoveAsync(typeId);
+        return Ok(new RemoveTypeResponse<int?>()
+        {
+            Id = res
+        });
     }
 }
