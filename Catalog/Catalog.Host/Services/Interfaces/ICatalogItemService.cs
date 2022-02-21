2cecbf3e3497c29f207cb1b5839d8f584caaf9cb
AntSenokosov <a.y.senokosov@gmail.com>
2022-02-21 13:09:00 +0200

    Edit ICatalogItemService.cs and ICatalogService.cs

--- Catalog/Catalog.Host/Services/Interfaces/ICatalogItemService.cs
+++ Catalog/Catalog.Host/Services/Interfaces/ICatalogItemService.cs
@@ -4,7 +4,7 @@ namespace Catalog.Host.Services.Interfaces;
 
 public interface ICatalogItemService
 {
-    Task<int?> Add(string name, string description, decimal price, int availableStock, int catalogBrandId, int catalogTypeId, string pictureFileName);
+    Task<int?> AddAsync(string name, string description, decimal price, int availableStock, int catalogBrandId, int catalogTypeId, string pictureFileName);
     Task<int?> UpdateAsync(UpdateProductRequest product);
     Task<int?> RemoveAsync(int itemId);
 }
--- Catalog/Catalog.Host/Services/Interfaces/ICatalogService.cs
+++ Catalog/Catalog.Host/Services/Interfaces/ICatalogService.cs
@@ -5,7 +5,7 @@ namespace Catalog.Host.Services.Interfaces;
 
 public interface ICatalogService
 {
-    Task<PaginatedItemsResponse<CatalogItemDto>> GetCatalogItemsAsync(int pageSize, int pageIndex);
+    Task<PaginatedItemsResponse<CatalogItemDto>?> GetCatalogItemsAsync(int pageSize, int pageIndex);
     Task<GetByIdResponse<CatalogItemDto>> GetCatalogItemByIdAsync(int id);
     Task<GetBrandsResponse<CatalogItemDto>> GetCatalogItemsByBrandAsync(int id);
     Task<GetTypesResponse<CatalogItemDto>> GetCatalogItemsByTypeAsync(int id);
