f3aef16080e5f3bbb5819d2979b0d425d64a90d3
vadimor <vadimorbilyu@gmail.com>
2022-02-21 14:32:01 +0200

    CatalogBrandRepository

---
+++ Catalog/Catalog.Host/Repositories/Interfaces/ICatalogBrandRepository.cs
@@ -0,0 +1,12 @@
+﻿using Catalog.Host.Data.Entities;
+
+namespace Catalog.Host.Repositories.Interfaces
+{
+    public interface ICatalogBrandRepository
+    {
+        Task<int?> AddAsync(string name);
+        Task<IEnumerable<CatalogBrand>> GetBrandsAsync();
+        Task RemoveAsync(int id);
+        Task<int?> UpdateAsync(int id, string name);
+    }
+}
